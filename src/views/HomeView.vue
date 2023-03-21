521f5a6b26125ec1aeec5d0ba884045cded1796c
prhyme <palln6935@gmail.com>
2023-03-21 20:42:59 +0530

    Clean out repo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,13 @@
 <template>
-	<HelloWorld />
+	<div class="home">
+		<h1>This is the home page</h1>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
 	name: "Home",
 
-	components: {
-		HelloWorld,
-	},
+	components: {},
 };
 </script>
