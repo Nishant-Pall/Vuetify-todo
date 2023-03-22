28f07cb3d1d429e2c035be74d2b759ad04b301c5
prhyme <palln6935@gmail.com>
2023-03-22 11:51:44 +0530

    Style font size and colors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,23 @@
 <template>
 	<div class="home">
 		<h1>This is the home page</h1>
+		<p class="red white--text">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+			repellendus tempore saepe assumenda id, quae odio vero dolor alias,
+			fugit atque harum nobis facere, laudantium omnis mollitia quos ullam
+			voluptatum?
+		</p>
+		<p class="pink lighten-4 blue--text text--darken-4">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+			distinctio amet excepturi. Modi in officia dolorum, laborum
+			reprehenderit debitis repudiandae illum eaque pariatur nisi
+			blanditiis optio nemo? Facilis, similique officia.
+		</p>
+		<h1 class="display-4">Massive Display</h1>
+		<h4 class="display-1">Small Display</h4>
 	</div>
 </template>
 
 <script>
-export default {
-	name: "Home",
-
-	components: {},
-};
+export default {};
 </script>
