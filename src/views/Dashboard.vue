7694ad36b93392b337601c9aef60a14f8e636dfc
prhyme <palln6935@gmail.com>
2023-03-28 22:14:21 +0530

    Create chips for project status

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -22,8 +22,13 @@
 						<div>{{ project.due }}</div>
 					</v-flex>
 					<v-flex xs2 sm4 md2>
-						<div class="caption grey--text">Status</div>
-						<div>{{ project.status }}</div>
+						<div class="right">
+							<v-chip
+								small
+								:class="`${project.status} white--text caption my-2`"
+								>{{ project.status }}</v-chip
+							>
+						</div>
 					</v-flex>
 				</v-layout>
 			</v-card>
@@ -87,38 +92,14 @@ button {
 .project.ongoing {
 	border-left: 4px solid tomato;
 }
+
+.v-chip.complete {
+	background: #3cd1c2 !important;
+}
+.v-chip.overdue {
+	background: orange !important;
+}
+.v-chip.ongoing {
+	background: tomato !important;
+}
 </style>
-
-<!-- <h1>This is the home page</h1>
-		<p class="red white--text">
-			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
-			repellendus tempore saepe assumenda id, quae odio vero dolor alias,
-			fugit atque harum nobis facere, laudantium omnis mollitia quos ullam
-			voluptatum?
-		</p>
-		<p class="pink lighten-4 blue--text text--darken-4">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-			distinctio amet excepturi. Modi in officia dolorum, laborum
-			reprehenderit debitis repudiandae illum eaque pariatur nisi
-			blanditiis optio nemo? Facilis, similique officia.
-		</p>
-		<h1 class="display-4 hidden-sm-and-down">Massive Display</h1>
-		<h4 class="display-1">Small Display</h4>
-
-		<v-btn class="pink white--text">click me</v-btn>
-		<v-btn color="pink" dark depressed>click me</v-btn>
-		<v-btn text color="pink">Click me</v-btn>
-
-		<v-btn depressed class="pink white--text">
-			<v-icon left>YO</v-icon>
-			<span>email me</span>
-		</v-btn>
-
-		<v-btn depressed small class="pink white--text">
-			<v-icon left small>YO</v-icon>
-			<span>email me</span>
-		</v-btn>
-
-		<v-btn depressed large class="white--text" color="purple" fab>
-			<span>me</span>
-		</v-btn> -->
