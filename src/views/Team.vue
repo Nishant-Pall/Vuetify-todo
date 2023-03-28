880720868926bcf8e8ff727e207e5e9367b07b9c
prhyme <palln6935@gmail.com>
2023-03-29 02:18:29 +0530

    Create avatar icons on team page

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -6,7 +6,9 @@
 			<v-layout row wrap>
 				<v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
 					<v-card text class="text-xs-center ma-3 pa-3">
-						<v-responsive class="pt-4"> image goes here </v-responsive>
+						<v-responsive class="pt-4">
+							<v-icon size="100"> mdi-account-circle </v-icon>
+						</v-responsive>
 						<v-card-text>
 							<div class="subheading">{{ person.name }}</div>
 							<div class="grey--text">{{ person.role }}</div>
